cdd65d5bbecae9a7a1308fac3790b24a4cec81f6
advanze <felipe@agile.cl>
2021-10-08 23:36:47 -0300

    End of proyect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Products from './components/Products';
+import Layout from './components/Layout'
+import Title from './components/Title';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  state = {
+    products: [
+      { name: 'Tomato', price: 10 , img: '/products/tomate.jpg'},
+      { name: 'Alberjas', price: 15 , img: '/products/arbejas.jpg'},
+      { name: 'Lechugas', price: 5 , img: '/products/lechuga.jpg'}
+    ],
+    cart: [],
+    isCartVisible: false,
+
+
+  }
+
+  addProductToCart = (product) => {    
+
+    const {cart} = this.state
+  
+    // Case 1: The product that is being added, already is on cart
+    if(cart.find( prodInCart => prodInCart.name === product.name)){
+      const newCart = cart.map(
+        prodInCart => prodInCart.name === product.name? 
+        ({
+          ...prodInCart,
+          quantity: prodInCart.quantity+ 1,
+        }) 
+        : prodInCart
+      )
+      return this.setState({cart: newCart})
+    }
+
+    // Case 2: The product that is being added, is not in the cart
+    return this.setState({
+      cart: this.state.cart.concat({
+        ...product,
+        quantity: 1,
+      })
+    })
+
+  }
+
+  showCart = () =>{
+    if(!this.state.cart.length){
+      return
+    }
+    this.setState({isCartVisible : !this.state.isCartVisible})
+  }
+
+  render(){
+    // console.log(this.state.cart)
+    const {isCartVisible} = this.state
+
+    return(
+      <div>
+        <Navbar 
+        cart={this.state.cart}
+        isCartVisible = {isCartVisible}
+        showCart = {this.showCart}
+        />
+
+        <Layout>
+          <Title/>
+          <Products 
+            addProductToCart={this.addProductToCart}
+            products = {this.state.products}>
+          </Products>
+
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
